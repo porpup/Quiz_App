517e5511926f1a6336eb9a5f0e608eb46d723b45
porpup <porpup@hotmail.com>
2023-10-05 11:38:16 -0400

    Implemented Login And Register Code to app

--- app/src/main/java/com/example/quiz_app/Login.java
+++ app/src/main/java/com/example/quiz_app/Login.java
@@ -5,26 +5,73 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
-    TextView register;
+    private EditText email, password;
+    private Button loginBtn;
+    private TextView register;
+    private FirebaseAuth firebaseAuthAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        email = findViewById(R.id.txtEmail);
+        password = findViewById(R.id.txtPassword);
+        loginBtn = findViewById(R.id.btnLogin);
         register = findViewById(R.id.txtRegister);
 
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Login.this, Register.class);
-                startActivity(intent);
-
-            }
+        register.setOnClickListener(view -> {
+            Intent intent = new Intent(Login.this, Register.class);
+            startActivity(intent);
+            finish();
         });
 
+        loginBtn.setOnClickListener(view -> {
+            loginUser();
+        });
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuthAuth.getCurrentUser();
+        if(currentUser != null) {
+            Intent intent = new Intent(Login.this, ChooseQuiz.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    private void loginUser() {
+        String email = this.email.getText().toString().trim();
+        String password = this.password.getText().toString().trim();
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Enter Email and Password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firebaseAuthAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful()) {
+                        Intent intent = new Intent(Login.this, ChooseQuiz.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(this, "Login Failed! Enter correct Email and Password", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
 }
--- app/src/main/java/com/example/quiz_app/Register.java
+++ app/src/main/java/com/example/quiz_app/Register.java
@@ -2,13 +2,81 @@ package com.example.quiz_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
 
 public class Register extends AppCompatActivity {
+    private EditText username, email, password, confirmPassword;
+    private Button registerBtn;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.txtUsername);
+        email = findViewById(R.id.txtEmail2);
+        password = findViewById(R.id.txtPassword2);
+        confirmPassword = findViewById(R.id.txtRepeatPassword);
+        registerBtn = findViewById(R.id.btnRegister);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        registerBtn.setOnClickListener(view -> {
+            registerUser();
+        });
+
     }
+
+    private void registerUser() {
+        String username = this.username.getText().toString();
+        String email = this.email.getText().toString();
+        String password = this.password.getText().toString();
+        String confirmPassword = this.confirmPassword.getText().toString();
+
+        if(username.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            Toast.makeText(this, "All the fields should be filled!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(!password.equals(confirmPassword)) {
+            Toast.makeText(this, "Passwords do not match!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(password.length() < 8) {
+            Toast.makeText(this, "Password should be at least 8 characters long!", Toast.LENGTH_SHORT).show();
+            this.password.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Please provide a valid email!", Toast.LENGTH_SHORT).show();
+            this.email.requestFocus();
+            return;
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if(task.isSuccessful()) {
+                Toast.makeText(this, "User registered successfully!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Register.this, Login.class);
+                intent.putExtra("username", username);
+                intent.putExtra("password", password);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(this, "Registration failed! Please try again.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
 }
